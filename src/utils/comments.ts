bd187e74bc4950f062d064cf36b696429ea67671
ramiro-monllor <ramiromonllor@gmail.com>
2024-07-15 14:33:50 -0300

    init frontend

---
+++ src/utils/comments.ts
@@ -0,0 +1,35 @@
+// Package Imports
+
+// UI imports
+
+// Component Imports
+
+// Config Imports
+
+// Custom Hook Imports
+
+// Helper Imports
+
+// Icon Imports
+
+// Style Imports
+
+// Util Imports
+
+// Export Component
+
+// Action Imports
+
+// Reducer Imports
+
+// Route Imports
+
+// Controller Imports
+
+// Model Imports
+
+// Interface Imports
+
+// Middleware Imports
+
+// DB Imports
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,5 @@
-import type { Config } from "tailwindcss";
-
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+export default {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -8,13 +7,18 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif']
       },
+      colors: {
+        brand: {
+          darkGray: "#1b1c20",
+          lightGrey: "#1a1c1f",
+          brown: "#651417",
+          red: "#A52029"
+        }
+      }
     },
   },
   plugins: [],
-};
-export default config;
+}
